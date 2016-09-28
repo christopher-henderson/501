0f063ef22f3602e76b0192b9a99b6c8bc21c404b
christopher-henderson <chris@chenderson.org>
2016-09-28 16:58:53 -0700

    max subarray done

--- assignment_2.py
+++ assignment_2.py
@@ -1,25 +1,23 @@
-# -*- coding: utf-8 -*-
-from numpy import *
-
-# TODO: Replace all TODO comments (yes, this one too!)
-
-
-
-#STOCK_PRICES  = [100,113,110,85,105,102,86,63,81,101,94,106,101,79,94,90,97]
-STOCK_PRICE_CHANGES =[13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7]
-
-
-
-
-#Implement pseudocode from the book
-def find_maximum_subarray_brute(A, low=0, high=-1):
+# -*- coding: utf-8 -*-from __future__ import division
+from numpy import *from itertools import combinations_with_replacement
+
+# STOCK_PRICES  = [100,113,110,85,105,102,86,63,81,101,94,106,101,79,94,90,97]
+STOCK_PRICE_CHANGES = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22,                    15, -4, 7]def find_maximum_subarray_brute(A, low=0, high=-1):
     """
     Return a tuple (i,j) where A[i:j] is the maximum subarray.
     Implement the brute force method from chapter 4
     time complexity = O(n^2)
-    """
-    #TODO
-
+    """    # combinations_with_replacement(range(len(A)), 2) builds all subarray    # indices.    # E.G. [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (1, 1), (1, 2), (1, 3),    #       (1, 4), (2, 2), (2, 3), (2, 4), (3, 3), (3, 4), (4, 4)]    # Such that we can iterate over those and select the maximum sum of A
+    # using these indices.    sub_arrays = combinations_with_replacement(range(len(A)), 2)
+    max_subarray = (0, 0)
+    max_sum = 0
+    for sub_array in sub_arrays:
+        this_sum = sum(A[sub_array[0]:sub_array[1] + 1])
+        if this_sum > max_sum:
+            max_subarray = sub_array
+            max_sum = this_sum
+    return max_subarray
+print(find_maximum_subarray_brute([1,2,3,-3,2]))
 
 
 #Implement pseudocode from the book
@@ -27,8 +25,21 @@ def find_maximum_crossing_subarray(A, low, mid,  high):
     """
     Find the maximum subarray that crosses mid
     Return a tuple (i,j) where A[i:j] is the maximum subarray.
-    """
-    #TODO
+    """    mE = mL = mR = 0
+    li = ri = 0
+    for index in range(mid, low - 1, -1):
+        mE += A[index]
+        if mE > mL:
+            mL = mE
+            li = index
+    mE = 0
+    for index in range(mid + 1, high + 1):
+        mE += A[index]
+        if mE > mR:
+            mR = mE
+            ri = index
+    return li, ri, mR + mL
+
 
 
 
@@ -36,8 +47,20 @@ def find_maximum_subarray_recursive(A, low=0, high=-1):
     """
     Return a tuple (i,j) where A[i:j] is the maximum subarray.
     Recursive method from chapter 4
-    """
-    #TODO
+    """    high = len(A) - 1 if high is -1 else high
+    if low == high:
+        return low, low, A[low]
+    mid = (low + high) // 2
+    li, lj, ls = find_maximum_subarray_recursive(A, low, mid)
+    ri, rj, rs = find_maximum_subarray_recursive(A, mid + 1, high)
+    ci, cj, cs = find_maximum_crossing_subarray(A, low, mid, high)
+    if ls >= rs and ls >= cs:
+        return (li, lj, ls)
+    if rs >= ls and rs >= cs:
+        return (ri, rj, rs)
+    if cs >= ls and cs >= rs:
+        return (ci, cj, cs)
+
 
 
 def find_maximum_subarray_iterative(A, low=0, high=-1):
@@ -58,9 +81,9 @@ def find_maximum_subarray_iterative(A, low=0, high=-1):
         if mE > mS:
             mS = mE
             mSij[0], mSij[1] = mEij[0], mEij[1]
-    return mSij,mS
+    return mSij
 
-print (find_maximum_subarray_iterative([1,2,3,-5,2]))
+# print (find_maximum_subarray_iterative([1,2,3,-5,2]))
 
 def square_matrix_multiply(A, B):
     """
