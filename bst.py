f4b68afc819cc418d3bc984dbb918fa645b9bfe0
christopher-henderson <chris@chenderson.org>
2016-10-07 15:57:21 -0700

    better bst

---
+++ bst.py
@@ -0,0 +1,283 @@
+class NullBinaryTreeNode(object):
+
+    SINGLETON = None
+
+    def __new__(cls):
+        if NullBinaryTreeNode.SINGLETON is not None:
+            return NullBinaryTreeNode.SINGLETON
+        NullBinaryTreeNode.SINGLETON = super(
+            NullBinaryTreeNode, cls).__new__(cls)
+        return NullBinaryTreeNode.SINGLETON
+
+    def __init__(self):
+        pass
+
+    def __bool__(self):
+        return False
+
+    def __nonzero__(self):
+        return False
+
+    def __gt__(self, other):
+        return False
+
+    def __lt__(self, other):
+        return False
+
+    def __ge__(self, other):
+        return False
+
+    def __le__(self, other):
+        return False
+
+    def __eq__(self, other):
+        return False
+
+    def __ne__(self, other):
+        return False
+
+    def __contains__(self, key):
+        return False
+
+    def __getitem__(self, key):
+        return None
+
+    @property
+    def parent(self):
+        return NullBinaryTreeNode.SINGLETON
+
+    @property
+    def left(self):
+        return NullBinaryTreeNode.SINGLETON
+
+    @property
+    def right(self):
+        return NullBinaryTreeNode.SINGLETON
+
+    @parent.setter
+    def parent(self, other):
+        return
+
+    @left.setter
+    def left(self, other):
+        return
+
+    @right.setter
+    def right(self, other):
+        return
+
+    def replace_child(self, child, replacement):
+        return
+
+    def delete(self, key):
+        raise Exception("Element {K} not found".format(K=key))
+
+    def add(self, key, data):
+        return BinaryTreeNode(key=key, data=data)
+
+    def inorder(self):
+        return iter(())
+
+    def preorder(self):
+        return iter(())
+
+    def postorder(self):
+        return iter(())
+
+    def height(self, height=0):
+        return height
+
+
+class BinaryTreeNode(object):
+    def __init__(
+            self,
+            data=None,
+            key=None,
+            left=NullBinaryTreeNode(),
+            right=NullBinaryTreeNode(),
+            parent=NullBinaryTreeNode()):
+        super(BinaryTreeNode, self).__init__()
+        self.key = key
+        self.data = data
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+    def add(self, key, data):
+        if self >= key:
+            self.left = self.left.add(key, data)
+            self.left.parent = self
+        elif self <= key:
+            self.right = self.right.add(key, data)
+            self.right.parent = self
+        return self
+
+    def inorder(self):
+        for k, v in self.left.inorder():
+            yield k, v
+        yield self.key, self.data
+        for k, v in self.right.inorder():
+            yield k, v
+
+    def postorder(self):
+        for k, v in self.left.inorder():
+            yield k, v
+        for k, v in self.right.inorder():
+            yield k, v
+        yield self.key, self.data
+
+    def preorder(self):
+        yield self.key, self.data
+        for k, v in self.left.inorder():
+            yield k, v
+        for k, v in self.right.inorder():
+            yield k, v
+
+    def height(self, height=-1):
+        height += 1
+        left_height = self.left.height(height)
+        right_height = self.right.height(height)
+        return max(left_height, right_height)
+
+    def __getitem__(self, key):
+        if self == key:
+            return self.data
+        elif self > key:
+            return self.left[key]
+        else:
+            return self.right[key]
+
+    def replace_child(self, child, replacement):
+        if self.left is child:
+            self.left = replacement
+        else:
+            self.right = replacement
+
+    def delete(self, key):
+        if self > key:
+            self.left.delete(key)
+            return self
+        if self < key:
+            self.right.delete(key)
+            return self
+        if self.left and self.right:
+            if self.right.height > self.left.height:
+                replacement = self.right.pop_minimum()
+            else:
+                replacement = self.left.pop_minimum
+            self.parent.replace_child(self, replacement)
+            replacement.parent = self.parent
+            if self.left is not replacement:
+                replacement.left = self.left
+            if self.right is not replacement.right:
+                replacement.right = self.right
+            return replacement
+        elif self.left and not self.right:
+            self.parent.replace_child(self, self.left)
+            self.left.parent = self.parent
+            return self.left
+        elif not self.left and self.right:
+            self.parent.replace_child(self, self.right)
+            self.right.parent = self.parent
+            return self.right
+        else:
+            self.parent.replace_child(self, self.right)
+            return self.right
+
+    def pop_minimum(self):
+        if self.left:
+            return self.left.pop_minimum()
+        self.parent.replace_child(self, self.left)
+        self.right.parent = self.parent
+        return self
+
+    def __gt__(self, other):
+        if isinstance(other, BinaryTreeNode):
+            return self.key > other.key
+        return self.key > other
+
+    def __lt__(self, other):
+        if isinstance(other, BinaryTreeNode):
+            return self.key < other.key
+        return self.key < other
+
+    def __ge__(self, other):
+        if isinstance(other, BinaryTreeNode):
+            return self.key >= other.key
+        return self.key >= other
+
+    def __le__(self, other):
+        if isinstance(other, BinaryTreeNode):
+            return self.key < other.key
+        return self.key <= other
+
+class BinarySearchTreeDict(object):
+
+    def __init__(self):
+        super(BinarySearchTreeDict, self).__init__()
+        self.root = NullBinaryTreeNode()
+        self.size = 0
+
+    @property
+    def height(self):
+        return self.root.height()
+
+    def inorder_keys(self):
+        return (k for k, _ in self.items())
+
+    def postorder_keys(self):
+        return (k for k, _ in self.root.postorder())
+
+    def preorder_keys(self):
+        return (k for k, _ in self.root.preorder())
+
+    def items(self):
+        return self.root.inorder()
+
+    def __getitem__(self, key):
+        return self.root[key]
+
+    def __setitem__(self, key, value):
+        self.root = self.root.add(key, value)
+
+    def __delitem__(self, key):
+        # TODO
+        self.root = self.root.delete(key)
+
+    def __contains__(self, key):
+        return key in self.root
+
+    def __len__(self):
+        return self.size
+
+    def display(self):
+        # TODO: Print the keys using INORDER on one
+        #      line and PREORDER on the next
+        return (
+            ", ".join(str(k) for k in self.inorder_keys()) + "\n" +
+            ", ".join(str(k) for k in self.preorder_keys())
+            )
+
+
+
+
+
+
+
+def main():
+    b = BinarySearchTreeDict()
+    b['i'] = 1
+    b['a'] = 2
+    b['b'] = 3
+    b['c'] = 3
+    b['d'] = 3
+    b['j'] = 3
+    print(b.display())
+    print(b.height)
+    del b["a"]
+    print(b.display())
+
+
+
+
+main()
---
+++ original.py
@@ -0,0 +1,272 @@
+#TODO: Get rid of all flake8 warnings -- that means adding docstrings
+#      to the file, classes, and methods.
+
+#=============================================================================
+
+class SinglyLinkedNode(object):
+
+    def __init__(self, item=None, next_link=None):
+        super(SinglyLinkedNode, self).__init__()
+        self._item = item
+        self._next = next_link
+
+    @property
+    def item(self):
+        return self._item
+
+    @item.setter
+    def item(self, item):
+        self._item = item
+
+    @property
+    def next(self):
+        return self._next
+
+    @next.setter
+    def next(self, next):
+        self._next = next
+
+    def __repr__(self):
+        return repr(self.item)
+
+
+class SinglyLinkedList(object):
+
+    def __init__(self):
+        super(SinglyLinkedList, self).__init__()
+        # TODO
+        pass
+
+    def __len__(self):
+        # TODO
+        pass
+
+    def __iter__(self):
+        # TODO
+        pass
+
+    def __contains__(self, item):
+        # TODO
+        pass
+
+    def remove(self, item):
+        # TODO: find item and remove it.
+        pass
+
+    def prepend(self, item):
+        # TODO ad item to the front of the list
+        pass
+
+    def __repr__(self):
+        s = "List:" + "->".join([item for item in self])
+        return s
+
+#=============================================================================
+
+class BinaryTreeNode(object):
+    def __init__(self, data = None, left = None, right = None, parent=None):
+        super(BinaryTreeNode, self).__init__()
+        self.data = data
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+
+class BinarySearchTreeDict(object):
+
+    def __init__(self):
+        super(BinarySearchTreeDict, self).__init__()
+
+        # TODO initialize
+        pass
+
+    @property
+    def height(self):
+        # TODO
+        pass
+
+    def inorder_keys(self):
+        # TODO:Use the 'yield'  keyword and StopIteration exception
+        # to return the keys, using an INORDER traversal
+        pass
+
+    def postorder_keys(self):
+        # TODO: Use 'yield' and 'StopIteration' to yield key in POSTORDER
+        pass
+
+    def preorder_keys(self):
+        # TODO: Use 'yield' and 'StopIteration' to yield key in PREORDER
+        pass
+
+    def items(self):
+        # TODO: Use 'yield' to return the items (key and value) using
+        # an INORDER traversal.
+        pass
+
+    def __getitem__(self, key):
+        # TODO: Get the VALUE associated with key
+        pass
+
+    def __setitem__(self, key, value):
+        # TODO:
+        pass
+
+    def __delitem(self, key):
+        # TODO
+        pass
+
+    def __contains__(self, key):
+        # TODO
+        pass
+
+    def __len__(self):
+        # TODO
+        pass
+
+    def display(self):
+        # TODO: Print the keys using INORDER on one
+        #      line and PREORDER on the next
+        pass
+
+#=============================================================================
+
+class ChainedHashDict(object):
+
+    def __init__(self, bin_count=10, max_load=0.7, hashfunc=hash):
+        super(ChainedHash, self).__init__()
+
+        # TODO: Construct a new table
+        pass
+
+    @property
+    def load_factor(self):
+        # TODO
+        pass
+
+    @property
+    def bin_count(self):
+        # TODO
+        pass
+
+    def rebuild(self, bincount):
+        # Rebuild this hash table with a new bin count
+        # TODO
+        pass
+
+    def __getitem__(self, key):
+        # TODO: Get the VALUE associated with key
+        pass
+
+    def __setitem__(self, key, value):
+        # TODO:
+        pass
+
+    def __delitem__(self, key):
+        # TODO
+        pass
+
+    def __contains__(self, key):
+        # TODO
+        pass
+
+    def __len__(self):
+        # TODO
+        pass
+
+    def display(self):
+        # TODO: Return a string showing the table with multiple lines
+        # TODO: I want the string to show which items are in which bins
+        pass
+
+
+class OpenAddressHashDict(object):
+
+    def __init__(self, bin_count=10, max_load=0.7, hashfunc=hash):
+        super(OpenAddressHashDict, self).__init__()
+
+        # TODO initialize
+        pass
+
+    @property
+    def load_factor(self):
+        # TODO
+        pass
+
+    @property
+    def bin_count(self):
+        # TODO
+        pass
+
+    def rebuild(self, bincount):
+        # Rebuild this hash table with a new bin count
+        # TODO
+        pass
+
+    def __getitem__(self, key):
+        # TODO: Get the VALUE associated with key
+        pass
+
+    def __setitem__(self, key, value):
+        # TODO:
+        pass
+
+    def __delitem(self, key):
+        # TODO
+        pass
+
+    def __contains__(self, key):
+        # TODO
+        pass
+
+    def __len__(self):
+        # TODO
+        pass
+
+    def display(self):
+        # TODO: Return a string showing the table with multiple lines
+        # TODO: I want the string to show which items are in which bins
+        pass
+
+
+
+def terrible_hash(bin):
+    """A terrible hash function that can be used for testing.
+
+    A hash function should produce unpredictable results,
+    but it is useful to see what happens to a hash table when
+    you use the worst-possible hash function.  The function
+    returned from this factory function will always return
+    the same number, regardless of the key.
+
+    :param bin:
+        The result of the hash function, regardless of which
+        item is used.
+
+    :return:
+        A python function that can be passed into the constructor
+        of a hash table to use for hashing objects.
+    """
+    def hashfunc(item):
+        return bin
+    return hashfunc
+
+
+def main():
+    # Thoroughly test your program and produce useful out.
+    #
+    # Do at least these kinds of tests:
+    #  (1)  Check the boundary conditions (empty containers,
+    #       full containers, etc)
+    #  (2)  Test your hash tables for terrible hash functions
+    #       that map to keys in the middle or ends of your
+    #       table
+    #  (3)  Check your table on 100s or randomly generated
+    #       sets of keys to make sure they function
+    #
+    #  (4)  Make sure that no keys / items are lost, especially
+    #       as a result of deleting another key
+    pass
+
+
+if __name__ == '__main__':
+    main()
