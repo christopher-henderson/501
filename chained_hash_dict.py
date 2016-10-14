6180ed4d4bb05f378e72c8e7a39533111bb06ba7
christopher-henderson <chris@chenderson.org>
2016-10-14 13:51:53 -0700

    chained hashing

---
+++ chained_hash_dict.py
@@ -0,0 +1,102 @@
+from __future__ import division
+
+from linked_list import SinglyLinkedList
+
+
+class ChainedHashDict(object):
+
+    def __init__(self, bin_count=10, max_load=0.7, hashfunc=hash):
+        super(ChainedHashDict, self).__init__()
+        # TODO: Construct a new table
+        if bin_count <= 0:
+            raise TypeError("Bin count must be greater than zero.")
+        self.table = [SinglyLinkedList() for _ in range(bin_count)]
+        self.max_load = max_load
+        self.hash = hashfunc
+        self.size = 0
+
+    @property
+    def load_factor(self):
+        return self.size / self.bin_count
+
+    @property
+    def bin_count(self):
+        return len(self.table)
+
+    @property
+    def should_resize(self):
+        return self.load_factor >= self.max_load
+
+    def rebuild(self, bincount):
+        # Rebuild this hash table with a new bin count
+        # TODO
+        tmp = self.table
+        self.table = [SinglyLinkedList() for _ in range(bincount)]
+        self.size = 0
+        for linked_list in tmp:
+            for node in linked_list:
+                self[node.key] = node.item
+        del tmp
+
+    def items(self):
+        for linked_list in self.table:
+            for node in linked_list:
+                yield node.key, node.value
+
+    def __getitem__(self, key):
+        # TODO: Get the VALUE associated with key
+        address = self._address(key)
+        chain = self.table[address]
+        for node in chain:
+            if node.key == key:
+                return node.item
+        return None
+
+    def __setitem__(self, key, value):
+        # TODO:
+        address = self._address(key)
+        for node in self.table[address]:
+            if node.key == key:
+                node.item = value
+                return
+        self.table[address].prepend(key=key, item=value)
+        self.size += 1
+        if self.should_resize:
+            self.rebuild(self.bin_count*2)
+
+    def __delitem__(self, key):
+        # TODO
+        address = self._address(key)
+        self.table[address].remove(key, lambda node, key: node.key == key)
+
+    def __contains__(self, key):
+        # TODO
+        address = self._address(key)
+        for node in self.table[address]:
+            if node.key == key:
+                return True
+        return False
+
+    def __len__(self):
+        # TODO
+        return self.size
+
+    def _address(self, key):
+        return self.hash(key) % self.bin_count
+
+    def display(self):
+        # TODO: Return a string showing the table with multiple lines
+        # TODO: I want the string to show which items are in which bins
+        print("{")
+        for index, linked_list in enumerate(self.table):
+            line = "\t[{BIN}]=> {LIST}".format(BIN=index, LIST=linked_list)
+            print(line)
+        print("}")
+
+
+m = ChainedHashDict()
+from random import randrange
+for _ in range(100):
+    m[randrange(-100, 100)] = randrange(-100, 100)
+m.display()
+print (len(m))
---
+++ linked_list.py
@@ -0,0 +1,96 @@
+class SinglyLinkedNode(object):
+
+    def __init__(self, key=None, item=None, next_link=None):
+        super(SinglyLinkedNode, self).__init__()
+        self._key = key
+        self._item = item
+        self._next = next_link
+
+    @property
+    def item(self):
+        return self._item
+
+    @item.setter
+    def item(self, item):
+        self._item = item
+
+    @property
+    def key(self):
+        return self._key
+
+    @key.setter
+    def key(self, key):
+        self._key = key
+
+    @property
+    def next(self):
+        return self._next
+
+    @next.setter
+    def next(self, next):
+        self._next = next
+
+    def __repr__(self):
+        return repr(self.item)
+
+
+class SinglyLinkedList(object):
+
+    def __init__(self):
+        super(SinglyLinkedList, self).__init__()
+        self.head = None
+        self.length = 0
+
+    def __len__(self):
+        # TODO
+        return self.length
+
+    def __iter__(self):
+        # TODO
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+
+    def __contains__(self, item):
+        # TODO
+        for node in self:
+            if node.item == item:
+                return True
+        return False
+
+    def remove(self, obj, comparator=lambda node, obj: node.item == obj):
+        previous = None
+        current = self.head
+        while current is not None:
+            found = comparator(current, obj)
+            if found and current is self.head:
+                value = self.head.item
+                self.head = self.head.next
+                self.length -= 1
+                return value
+            elif found:
+                previous.next = current.next
+                self.length -= 1
+                return current.item
+            else:
+                previous = current
+                current = current.next
+        return None
+
+    def prepend(self, item, key=None):
+        # TODO ad item to the front of the list
+        node = SinglyLinkedNode(item=item, key=key)
+        if self.head is None:
+            self.head = node
+        else:
+            node.next = self.head
+            self.head = node
+        self.length += 1
+
+    def delete_by_key(self, key):
+        pass
+
+    def __repr__(self):
+        s = "List: " + " -> ".join([str(item) for item in self])
+        return s
