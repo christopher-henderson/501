c267fe9fe3470f9031e06c8f23ff1652413d0647
christopher-henderson <chris@chenderson.org>
2016-11-24 22:34:12 -0700

    more style

--- assignment5/knapsack.py
+++ assignment5/knapsack.py
@@ -13,21 +13,13 @@ def knapsack(max_size, items):
         return known[size]
     return knapsack_closure(max_size)
 
-    # def knapsack_closure(size):
-    #     known[size] = known[size] if known[size] is not None else max(
-    #         item[1] + knapsack_closure(size - item[0]) if
-    #         size - item[0] >= 0 else 0
-    #         for item in items)
-    #     return known[size]
-    # return knapsack_closure(max_size)
-
 # items = ((12, 4), (2, 2), (1, 2), (1, 1), (4, 10))
 # print (knapsack(8, items))
 
-# items = ((2, 3), (3, 4), (4, 5), (5, 8), (9, 10))
-# start = time()
-# print(knapsack(20, items))
-# print(time() - start)
+items = ((2, 3), (3, 4), (4, 5), (5, 8), (9, 10))
+start = time()
+print(knapsack(20, items))
+print(time() - start)
 
 # items = ((10, 60), (20, 100), (30, 120))
 # print(knapsack(50, items))
